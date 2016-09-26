c1ca9d62f9229bee4574441f33ba990db487e9d0
ericwang1902 <ericwang1902@163.com>
2016-09-26 16:56:23 +0800

    'create'

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/hello.vue'
+
+export default {
+  components:{
+    Hello
+  },
+  data () {
+    return {
+      msg: 'Hello Vue 2.0!'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
